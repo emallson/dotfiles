9b0c0780a0b27898096bdbbadc37ad6c79a97eb6
J David Smith <emallson@archlinux.us>
2014-03-24 09:31:46 -0400

    Updated manage.sh

--- manage.sh
+++ manage.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 if [[ $1 == "out" ]]; then
     rsync -avz --exclude-from=excludes ./ ~/;
@@ -7,7 +7,7 @@ elif [[ $1 == "in" ]]; then
     rsync -avz --existing --delete --exclude-from=excludes ~/ ./;
 elif [[ $1 == "add" ]]; then
     CWD=$(pwd);
-    cd ~;
+    cd $HOME;
     cp --parents "${2#$HOME/}" "${CWD}/";
     cd $CWD;
     git add "${2#$HOME/}";
