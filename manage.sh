2b2a25632f3f618566ef1c43f5c50a7d180dae67
J David Smith <emallson@archlinux.us>
2014-04-14 09:08:32 -0400

    Added 'refresh' command to manage.sh. refresh is like 'in' but does not delete.

--- manage.sh
+++ manage.sh
@@ -5,6 +5,8 @@ if [[ $1 == "out" ]]; then
     echo;
 elif [[ $1 == "in" ]]; then
     rsync -avz --existing --delete --exclude-from=excludes ~/ ./;
+elif [[ $1 == "refresh" ]]; then
+    rsync -avz --existing --exclude-from=excludes ~/ ./;
 elif [[ $1 == "add" ]]; then
     CWD=$(pwd);
     cd $HOME;
