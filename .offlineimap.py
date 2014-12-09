bcbfdd8cc9076a5a08072079d801d5b49c1ce45a
J David Smith <emallson@archlinux.us>
2014-12-09 17:15:20 -0500

    Added offlineimap config

---
+++ .offlineimap.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+import re, os
+
+def get_authinfo(machine, port):
+    l = "machine {machine} login (?P<login>[^\"]*?) port {port} password \"(?P<password>[^\"]*)\"".format(machine=machine, port=port)
+    p = re.compile(l)
+    authinfo = os.popen("gpg -q --no-tty -d ~/.authinfo.gpg").read()
+    return dict(zip(["username", "password"], p.search(authinfo).groups()))
