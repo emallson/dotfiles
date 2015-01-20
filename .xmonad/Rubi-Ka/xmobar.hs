11f4a0f1d601119e3f3b40414ac3dcb00fbb3429
J David Smith <emallson@archlinux.us>
2015-01-20 10:35:49 -0500

    Moved disk usage to left side so that the mpd monitor doesn't overwrite it.

--- .xmonad/Rubi-Ka/xmobar.hs
+++ .xmonad/Rubi-Ka/xmobar.hs
@@ -12,7 +12,7 @@ Config { commands = [ Run Wireless "wlp3s0" ["-t", "<essid> <quality>"
                                 ["-L", "50", "-H", "75", "-m", "1", "-p", "3",
                                  "--normal", "#F0DFAF", "--high", "#CC9393", "--low", "#7F9F7F"] 20
                     ]
-       , template = "%StdinReader% } %mpd% { %disku% | %wlp3s0wi% | %battery% | %date%"
+       , template = "%StdinReader% | %disku% } %mpd% { %wlp3s0wi% | %battery% | %date%"
        , bgColor = "#202020"
        , position = Bottom
        , alignSep = "}{"
