4610f937675a47ce293a3239db060124f58c0adf
J David Smith <emallson@archlinux.us>
2015-12-08 13:47:38 -0500

    Work on mu4e

--- .offlineimap.py
+++ .offlineimap.py
@@ -6,3 +6,19 @@ def get_authinfo(machine, port):
     p = re.compile(l)
     authinfo = os.popen("gpg2 -q --no-tty -d ~/.authinfo.gpg").read()
     return dict(zip(["username", "password"], p.search(authinfo).groups()))
+
+def sub(mapping):
+    def _sub(s):
+        try:
+            return mapping[s]
+        except:
+            return s
+    return _sub
+
+def rsub(mapping):
+    return sub({v: k for k, v in mapping.items()})
+
+def ignore(l):
+    def _i(s):
+        return s not in l
+    return _i
