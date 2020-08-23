6bab846a10056012512a9cbedb6b14db5d9a9dce
David Smith <emallson@emallson.net>
2020-08-23 17:38:43 -0400

    updated lein config

--- clojure/.lein/profiles.clj
+++ clojure/.lein/profiles.clj
@@ -1,2 +1,5 @@
-{:user {:plugins [[jonase/eastwood "0.2.3"]
-                  [lein-kibit "0.1.2"]]}}
+{:user {:plugins [[jonase/eastwood "0.3.11"]
+                  [lein-kibit "0.1.8"]
+                  [lein-cljfmt "0.6.7"]
+                  [cider/cider-nrepl "0.25.3"]
+                  [refactor-nrepl "2.5.0"]]}}
